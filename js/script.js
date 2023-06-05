b160ca3f6bc939f0eeb3597dd1d6db34547de3cf
Carlos Eduardo <2005carloseduardosantos@gmail.com>
2023-06-05 10:35:37 -0300

    2023

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const clouds = document.querySelector('.clouds');
 
 const jump = () => {
     mario.classList.add('jump');
@@ -19,14 +20,18 @@ const loop = setInterval(() => {
     if (pipePosition <= 120 && pipePosition > 0  && marioPosition < 80) {
 
         pipe.style.animation ='none';
-        pipe.style.left = `${pipePosition}px`;  
+        pipe.style.left = `${pipePosition}px`;
+        clouds.style.animation = 'none'
 
         mario.style.animation ='none';
         mario.style.bottom = `${marioPosition}px`;  
+        mario.style.animation ='none';
 
         mario.src = 'imgs/game-over.png'
         mario.style.width = '75px'
         mario.style.marginLeft = '50px'
+
+        
 
         clearInterval(loop);
     }
